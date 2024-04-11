60632a73ed3f917f7100a20855f32f1e8e4ec3c7
Hooman Keshvari <hm.keshvari@gmail.com>
2024-04-11 21:23:15 +0000

    added open syscall ( not finished yet) + little fix

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,15 +5,30 @@
 #include "threads/thread.h"
 #include "devices/shutdown.h"
 #include "lib/kernel/console.h"
+#include "filesys/filesys.h"
+// #include "filesys/file.h"
+// #include "lib/kernel/list.h"
 
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 
 static void syscall_handler (struct intr_frame *);
 
+//maybe we do not need it but lets see
+struct file_item {
+  int fd;
+  void* file;
+  struct list_elem elem;
+};
+
+struct list all_files;
+int fd_num;
+
 void
 syscall_init (void)
 {
+  fd_num = 2;
+  list_init(&all_files);
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -55,4 +70,41 @@ syscall_handler (struct intr_frame *f UNUSED)
 
       }
   }
+  else if (args[0] == SYS_OPEN) {
+    char *file_name = (char *)(void *)args[1];
+    if (file_name == NULL){
+	    f -> eax = -1;
+	    return;
+    }
+    struct file* file_to_open = filesys_open(file_name);
+    int fd = -1;
+    if(file_to_open == NULL){
+	
+    } else {
+    	struct file_item *new_file = malloc(sizeof(struct file_item));
+   	  new_file -> fd = fd_num;
+    	new_file -> file = file_to_open;
+    	list_push_back(&all_files, &new_file->elem);
+      fd = fd_num;
+      fd_num ++;
+    }
+    f -> eax = fd;
+  }
 }
+
+
+/* 
+  
+ Quick thing for myself : use this for list itteration ( probably would be needed )
+
+    struct file_item *new_file2 = malloc(sizeof(struct file_item));
+    new_file2 -> fd = 31;
+    new_file2 -> file = file_to_open;
+    list_push_back(&all_files, &new_file2->elem);
+
+    struct list_elem *e;
+    for (e = list_begin(&all_files); e != list_end(&all_files); e = list_next(e)) {
+       struct file_item *f = list_entry(e, struct file_item, elem);
+       printf("%d ----- %p \n", f->fd, f->file);
+    }
+*/
